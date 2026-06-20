4a776d240a382bdfd55da585efae87176f856fee
Sneha Pagare <154441020+snehapagare1212@users.noreply.github.com>
2026-06-20 20:27:44 +0530

    all changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Program from './Components/Program/Program'
-import Dd from './Components/DD/Dd'
+import Title from './Components/Title/Title'
+
 
 function App() {
 
   return (
     <>
-<Navbar/>
-<Hero/>
+      <Navbar />
+      <Hero />
 
 
-<div className="container">
-<Program/>  
-<Dd/>
-</div>
+      <div className="container">
+        <Title/>
+        <Program />
+      
+      </div>
 
-     </>
+    </>
   )
 }
 
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css'
+const Title = () => {
+  return (
+    <div className='title'>
+        <p>Our Product</p>
+        <h2>We Provide Master Courses </h2>
+      
+    </div>
+  )
+}
+
+export default Title
